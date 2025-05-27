7e110dac5d5734eba053336293d2fb597fdcbf27
hainguyen <nguyenhaia8@gmail.com>
2025-05-26 21:17:55 -0500

    prog 2

--- lab2/Prog2.java
+++ lab2/Prog2.java
@@ -1,7 +1,20 @@
 package lab2;
 
 public class Prog2 {
+    public class RandomNumbers {
+        public static int getRandomInt(int low, int high) {
+            return (int)(Math.random() * (high - low + 1)) + low;
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Prog2");
+
+        int x = RandomNumbers.getRandomInt(1, 9);
+        double result1 = Math.pow(Math.PI, x);
+        System.out.println("PI raised to the power of " + x + " is: " + result1);
+
+        int y = RandomNumbers.getRandomInt(3, 14);
+        double result2 = Math.pow(y, Math.PI);
+        System.out.println(y + " raised to the power of PI is: " + result2);
     }
 }
