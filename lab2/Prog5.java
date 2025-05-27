059b71a05fcce51b566d2d74a42bc7f38ce636d3
adisalem <as227ne@student.lnu.se>
2025-05-27 14:27:59 -0500

    format prog5

--- lab2/Prog5.java
+++ lab2/Prog5.java
@@ -2,17 +2,13 @@ import java.util.Scanner;
 
 public class Prog5 {
 
-    public static void main(String [] args)
-    {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.next();
-        for(int i=input.length()-1;i>=0;i--){
-                System.out.print(input.charAt(i));
-            }
+        for (int i = input.length() - 1; i >= 0; i--) {
+            System.out.print(input.charAt(i));
+        }
         System.out.println();
-
         sc.close();
-
     }
-    
 }
