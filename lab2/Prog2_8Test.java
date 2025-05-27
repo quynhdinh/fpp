c9eb66e25c92ef6926f08c3a1b23607d6d0bff95
adisalem <as227ne@student.lnu.se>
2025-05-27 14:34:03 -0500

    rename prog8

--- lab2/Prog2_8Test.java
+++ lab2/Prog2_8Test.java
@@ -7,7 +7,7 @@ public class Prog2_8Test {
     public void testMin() {
         int[] input = {2, -21, 3, 45, 0, 12, 18, 6, 3, 1, 0, -22};
         int expectedMin = -22;
-        int actualMin = Prog8.min(input);
+        int actualMin = Prog2_8.min(input);
         assertEquals(expectedMin, actualMin);
     }
 }
