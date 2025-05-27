d5302034cf3fa27f0936834d19af52c6210a15f4
adisalem <as227ne@student.lnu.se>
2025-05-26 19:28:38 -0500

    prog3:  summing up floats to int by using (int) cast and Math.round

---
+++ Prog3.java
@@ -0,0 +1,15 @@
+public class Prog3 {
+    public static void main(String[] args) {
+       float a = 1.27f;
+       float b = 3.881f;
+       float c = 9.6f;
+       //sum by casting to int
+       int sum1 = (int)(a+b+c);
+       // sum by using Math.round
+       int sum2 = Math.round(a+b+c);
+      
+       System.out.println("Sum by casting to int: " + sum1);
+       System.out.println("Sum by Math.round: " + sum2);
+       
+    }
+}
