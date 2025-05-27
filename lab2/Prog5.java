0060a3e38dd2489a83de03983f51003c65748630
adisalem <as227ne@student.lnu.se>
2025-05-27 01:37:04 -0500

    modified to avoid string object

--- lab2/Prog5.java
+++ lab2/Prog5.java
@@ -6,16 +6,13 @@ public class Prog5 {
     {
         Scanner sc = new Scanner(System.in);
         String input = sc.next();
-        System.out.println(reverseString(input));
+        for(int i=input.length()-1;i>=0;i--){
+                System.out.print(input.charAt(i));
+            }
+        System.out.println();
+
+        sc.close();
 
     }
-
-    private static String reverseString(String input){
-     String reversestr = "";
-     for(int i=0;i<input.length();i++){
-         reversestr = input.charAt(i) + reversestr;
-     }
-
-     return reversestr;
-    }
+    
 }
