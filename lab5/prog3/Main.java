2ffaa42cedf3775351e588f056703de0d6f061d0
hainguyen <nguyenhaia8@gmail.com>
2025-06-02 18:37:49 -0500

    ascending

--- lab5/prog3/Main.java
+++ lab5/prog3/Main.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        StringSort ss = new StringSort(new StringLengthComparator(false));
+        StringSort ss = new StringSort(new StringLengthComparator(true));
         String[] testStrings = {"andy", "mike", "joe", "allen", "stephan"};
         ss.stringSort(testStrings);
         System.out.println(Arrays.asList(testStrings));
