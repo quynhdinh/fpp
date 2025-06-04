eedb5165fab01660bbdb0fd7b3b00f0d12a1a693
hainguyen <nguyenhaia8@gmail.com>
2025-06-04 14:18:54 -0500

    remove space

--- lab7/prog3/Merge.java
+++ lab7/prog3/Merge.java
@@ -1,23 +1,20 @@
-package lab7.prog3;
+package prog3;
 
 import java.util.LinkedList;
 
 public class Merge {
     public LinkedList<Integer> merge(LinkedList<Integer> list1, LinkedList<Integer> list2) {
         LinkedList<Integer> list3 = new LinkedList<>();
-
         if (list1.isEmpty()) {
             list3.addAll(list2);
             return list3;
         }
-
         if (list2.isEmpty()) {
             list3.addAll(list1);
             return list3;
         }
         int first1 = list1.getFirst();
         int first2 = list2.getFirst();
-
         if (first1 < first2) {
             list1.removeFirst();
             list3.add(first1);
@@ -25,15 +22,12 @@ public class Merge {
             list2.removeFirst();
             list3.add(first2);
         }
-
-        // Recursive call and combine
         list3.addAll(merge(list1, list2));
         return list3;
     }
 
     public static void main(String[] args) {
         Merge merger = new Merge();
-
         LinkedList<Integer> list1 = new LinkedList<>();
         list1.add(2);
         list1.add(5);
@@ -44,7 +38,6 @@ public class Merge {
         list2.add(1);
         list2.add(3);
         list2.add(6);
-
         LinkedList<Integer> merged = merger.merge(list1, list2);
         System.out.println("Merged list: " + merged);
     }
