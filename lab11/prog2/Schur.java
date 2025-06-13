42dfc7993811bf66154ae2f15eb67d9335c38802
quynhdv <quynhvdinh@gmail.com>
2025-06-13 13:52:16 -0500

    init lab 11

---
+++ lab11/prog2/Schur.java
@@ -0,0 +1,12 @@
+package lab11.prog2;
+
+import java.util.List;
+
+public class Schur {
+    boolean checkForSum(List<Integer> list, Integer z) {
+        return false;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
