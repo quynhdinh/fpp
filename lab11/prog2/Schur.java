1ff31ce658ba9e8ce079b3197e840d6966afd768
adisalem <adislem@gmail.com>
2025-06-13 14:13:27 -0500

    lab11-2 done

--- lab11/prog2/Schur.java
+++ lab11/prog2/Schur.java
@@ -1,12 +1,33 @@
 package lab11.prog2;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class Schur {
     boolean checkForSum(List<Integer> list, Integer z) {
+        if (list == null || list.size() < 2) {
+            return false;
+        }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < list.size(); i++) {
+            Integer x = list.get(i);
+            int y = z - x;
+            if (map.containsKey(y)) {
+                return true;
+            }
+            map.put(x, i);
+        }
         return false;
     }
+
     public static void main(String[] args) {
-        
+        Schur schur = new Schur();
+        List<Integer> list = List.of(9, 8, 20, 8, 2, 5);
+        Integer z = 7;
+
+        boolean result = schur.checkForSum(list, z);
+        System.out.printf("Does the list (%s)contain two numbers that sum to " + z + "? " + result, list);
+
     }
 }
